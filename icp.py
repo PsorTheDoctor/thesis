edae15f453d0efb0cf6897d3f9b32802e29e2d08
PsorTheDoctor <40136368+PsorTheDoctor@users.noreply.github.com>
2022-01-21 02:40:16 +0100

    Add files via upload

---
+++ icp.py
@@ -0,0 +1,26 @@
+def icp(A, B, max_iterations, tolerance=0.001):
+
+  n = A.shape[1]
+
+  A1 = np.ones((n + 1, A.shape[0]))
+  B1 = np.ones((n + 1, B.shape[0]))
+  A1[:n, :] = np.copy(A.T)
+  B1[:n, :] = np.copy(B.T)
+  
+  prev_error = 0
+  for i in range(iterations):
+
+    distances, indices = calculate_distance(A1[:n, :].T,
+                                            B1[:n, :].T)
+
+    T = transform_matrix(A1[:n, :].T, B1[:n, indices].T)
+    
+    A1 = np.dot(T, A1)
+
+    mean_error = np.mean(distances)
+    if abs(prev_error - mean_error) < tolerance:
+      break
+    prev_error = mean_error
+
+  T = transform_matrix(A, A1[:n, :].T)
+  return T
---
+++ transform_matrix.py
@@ -0,0 +1,21 @@
+def transform_matrix(A, B):
+
+  n = A.shape[1]
+
+  centroid_A = np.mean(A, axis=0)
+  centroid_B = np.mean(B, axis=0)
+
+  A1 = A - centroid_A
+  B1 = B - centroid_B
+
+  H = np.dot(A1.T, B1)
+  U, D, Vt = np.linalg.svd(H)
+  R = np.dot(Vt.T, U.T)
+
+  if np.linalg.det(R) < 0:
+    Vt[n - 1, :] *= -1
+    R = np.dot(Vt.T, U.T)
+
+  t = centroid_B.T - np.dot(R, centroid_A.T)
+
+  return R, t
